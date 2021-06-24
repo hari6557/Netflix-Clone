3866c9072477caa8a8d3f1f54bc11c0f096c8540
Harikrishnan s <harikrishnansajikumar123@gmail.com>
2021-06-25 03:00:34 +0530

    Updates

---
+++ precache-manifest.72d256b0a19112a00ab99e38ecdd488b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c428bfb7220c88856815dc1d16a5338f",
+    "url": "/Netflix-Clone/index.html"
+  },
+  {
+    "revision": "73601d50e417cdedf72d",
+    "url": "/Netflix-Clone/static/css/main.da2c198b.chunk.css"
+  },
+  {
+    "revision": "46f173da6c3f7f499960",
+    "url": "/Netflix-Clone/static/js/2.83426871.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflix-Clone/static/js/2.83426871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73601d50e417cdedf72d",
+    "url": "/Netflix-Clone/static/js/main.49b1a685.chunk.js"
+  },
+  {
+    "revision": "b2b8b24758047419b104",
+    "url": "/Netflix-Clone/static/js/runtime-main.f28e37b4.js"
+  }
+]);
